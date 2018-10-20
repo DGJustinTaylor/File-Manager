feb3ae068b16b4effb0031c863a41dd4feb9b120
Justin Taylor <DGJustinTaylor@yahoo.com>
2018-10-20 02:16:23 -0400

    Basic funtionality added
    Can read from text files, and gathers all files from a specified path of a single type

---
+++ TxtEditor_TxtHolder/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmFileOrg
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmbFileList = New System.Windows.Forms.ComboBox()
+        Me.txtDirectory = New System.Windows.Forms.TextBox()
+        Me.txtFileType = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnFillList = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.slblProgInfo = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.btnRead = New System.Windows.Forms.Button()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'cmbFileList
+        '
+        Me.cmbFileList.FormattingEnabled = True
+        Me.cmbFileList.Location = New System.Drawing.Point(12, 64)
+        Me.cmbFileList.Name = "cmbFileList"
+        Me.cmbFileList.Size = New System.Drawing.Size(260, 21)
+        Me.cmbFileList.TabIndex = 0
+        '
+        'txtDirectory
+        '
+        Me.txtDirectory.Location = New System.Drawing.Point(92, 12)
+        Me.txtDirectory.Name = "txtDirectory"
+        Me.txtDirectory.Size = New System.Drawing.Size(180, 20)
+        Me.txtDirectory.TabIndex = 1
+        '
+        'txtFileType
+        '
+        Me.txtFileType.Location = New System.Drawing.Point(92, 38)
+        Me.txtFileType.Name = "txtFileType"
+        Me.txtFileType.Size = New System.Drawing.Size(180, 20)
+        Me.txtFileType.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(34, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Directory:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(11, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(75, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "File Extension:"
+        '
+        'btnFillList
+        '
+        Me.btnFillList.Location = New System.Drawing.Point(12, 91)
+        Me.btnFillList.Name = "btnFillList"
+        Me.btnFillList.Size = New System.Drawing.Size(125, 30)
+        Me.btnFillList.TabIndex = 5
+        Me.btnFillList.Text = "Fill List"
+        Me.btnFillList.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.slblProgInfo})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 137)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(284, 22)
+        Me.StatusStrip1.TabIndex = 6
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'slblProgInfo
+        '
+        Me.slblProgInfo.Name = "slblProgInfo"
+        Me.slblProgInfo.Size = New System.Drawing.Size(120, 17)
+        Me.slblProgInfo.Text = "ToolStripStatusLabel1"
+        '
+        'btnRead
+        '
+        Me.btnRead.Location = New System.Drawing.Point(147, 91)
+        Me.btnRead.Name = "btnRead"
+        Me.btnRead.Size = New System.Drawing.Size(125, 30)
+        Me.btnRead.TabIndex = 7
+        Me.btnRead.Text = "Read File"
+        Me.btnRead.UseVisualStyleBackColor = True
+        '
+        'frmFileOrg
+        '
+        Me.AcceptButton = Me.btnFillList
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 159)
+        Me.Controls.Add(Me.btnRead)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.btnFillList)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtFileType)
+        Me.Controls.Add(Me.txtDirectory)
+        Me.Controls.Add(Me.cmbFileList)
+        Me.Name = "frmFileOrg"
+        Me.Text = "Text File Organizer"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cmbFileList As ComboBox
+    Friend WithEvents txtDirectory As TextBox
+    Friend WithEvents txtFileType As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnFillList As Button
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents slblProgInfo As ToolStripStatusLabel
+    Friend WithEvents btnRead As Button
+End Class
---
+++ TxtEditor_TxtHolder/Form1.vb
@@ -0,0 +1,61 @@
+﻿'Programmer: Justin Taylor
+'Date: 10/20/2018
+'Purpose: Started as a project to help with the endless clutter of .txt files, is turning into much more.
+
+Option Strict On
+Option Explicit On
+
+Public Class frmFileOrg
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Simply give my information
+        slblProgInfo.Text = "Programmer: " & "Justin Taylor " & " | " & "Date: " & Today
+    End Sub
+
+    Private Sub btnFillList_Click(sender As Object, e As EventArgs) Handles btnFillList.Click
+        'Fill list with desired file type from given path
+        Dim filePath As String
+        Dim fileType As String
+
+        cmbFileList.Items.Clear()
+
+        If txtDirectory.Text = "" Then
+            MessageBox.Show("Directory input required.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        ElseIf txtFileType.Text = "" Then
+            MessageBox.Show("File type input required.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            filePath = txtDirectory.Text
+            fileType = txtFileType.Text
+
+            GetFiles(filePath, fileType)
+        End If
+    End Sub
+
+    Private Sub btnRead_Click(sender As Object, e As EventArgs) Handles btnRead.Click
+        'Call the read file function when clicked
+        ReadFile()
+    End Sub
+
+    Private Sub GetFiles(ByVal path As String, ByVal fileType As String)
+        'find all files of a certain type and put them in the drop down list
+        Dim di As New System.IO.DirectoryInfo(path)
+        Dim fi As System.IO.FileInfo
+
+        For Each fi In di.GetFiles
+            If fi.Extension = fileType Then
+                cmbFileList.Items.Add(fi.FullName)
+            End If
+        Next
+
+        cmbFileList.SelectedIndex = 0
+    End Sub
+
+    Private Sub ReadFile()
+        'Read the file
+        'TODO: add validation for only reading if the file type is .txt
+        Dim thePath As String = cmbFileList.SelectedItem.ToString()
+        Dim theText As String
+
+        theText = My.Computer.FileSystem.ReadAllText(thePath)
+        MessageBox.Show(theText, "Text in file", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+End Class
